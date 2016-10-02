cfa9571cc9e0c1d277508b336790f0632cab8ca6
Gaudy <gbm2494@hotmail.com>
2016-10-02 12:32:09 -0600

    cambios finales

---
+++ EjemploJS/EjemploJS/Controllers/personasController.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using EjemploJS.Models;
+
+namespace EjemploJS.Controllers
+{
+    public class personasController : Controller
+    {
+        private Entities db = new Entities();
+
+        // Método get para la vista unificada
+        public ActionResult VistUnificada(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            persona persona = db.persona.Find(id);
+            if (persona == null)
+            {
+                return HttpNotFound();
+            }
+            return View(persona);
+        }
+
+        //Método para Javascript para eliminar una persona, se llama en el jquery
+        public ActionResult eliminarPersona(string id)
+        {
+
+            persona persona = db.persona.Find(id);
+            db.persona.Remove(persona);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        // Método post de la vista Unificada, se llama unicamente en el botón Guardar de la sección modificar
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult VistUnificada([Bind(Include = "nombre,apellido1,apellido2,cedula,carne,sexo,fechaNac,email")] persona persona)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(persona).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(persona);
+        }
+
+        ///--------------------------------------------------------------------------------------------------------->
+        //------------------------------------METODOS GENERADOS AUTOMATICAMENTE ---------------------------------->
+        // GET: personas
+        public ActionResult Index()
+        {
+            return View(db.persona.ToList());
+        }
+
+        // GET: personas/Details/5
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            persona persona = db.persona.Find(id);
+            if (persona == null)
+            {
+                return HttpNotFound();
+            }
+            return View(persona);
+        }
+
+
+        // GET: personas/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: personas/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "nombre,apellido1,apellido2,cedula,carne,sexo,fechaNac,email,id")] persona persona)
+        {
+            if (ModelState.IsValid)
+            {
+                db.persona.Add(persona);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(persona);
+        }
+
+        // GET: personas/Edit/5
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            persona persona = db.persona.Find(id);
+            if (persona == null)
+            {
+                return HttpNotFound();
+            }
+            return View(persona);
+        }
+
+        // POST: personas/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "nombre,apellido1,apellido2,cedula,carne,sexo,fechaNac,email,id")] persona persona)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(persona).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(persona);
+        }
+
+        // GET: personas/Delete/5
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            persona persona = db.persona.Find(id);
+            if (persona == null)
+            {
+                return HttpNotFound();
+            }
+            return View(persona);
+        }
+
+        // POST: personas/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            persona persona = db.persona.Find(id);
+            db.persona.Remove(persona);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+
+    }
+}
